0e43e16fe321d54f0016e5c777cd7c312ecce7c4
a_android_a <androidnal@yandex.ru>
2026-02-25 12:08:23 +0300

    feat: add JSON message handling to SslServer

--- Server/SslServer.cpp
+++ Server/SslServer.cpp
@@ -3,6 +3,12 @@
 #include <QSslKey>
 #include <QSslCertificate>
 #include <QSslSocket>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonParseError>
+#include <QSqlQuery>
+
 SslServer::SslServer(QObject *parent)
     : QTcpServer(parent)
 {
@@ -23,7 +29,7 @@ void SslServer::incomingConnection(qintptr socketDescriptor)
 
     QFile keyFile("server.key");
     if (!keyFile.open(QIODevice::ReadOnly)) {
-        qWarning() << "Error: the server.key was not found in the current folder!";
+        qWarning() << "Mistake: the server.key was not found in the current folder!";
         delete sslSocket;
         return;
     }
@@ -37,13 +43,13 @@ void SslServer::incomingConnection(qintptr socketDescriptor)
 
     QFile certFile("server.crt");
     if (!certFile.open(QIODevice::ReadOnly)) {
-        qWarning() << "Error: the server.crt was not found in the current folder!";
+        qWarning() << "Mistake: the server.crt was not found in the current folder!";
         delete sslSocket;
         return;
     }
     QSslCertificate cert(certFile.readAll());
     if (cert.isNull()) {
-        qWarning() << "Error: invalid certificate!";
+        qWarning() << "Error: Invalid certificate!";
         delete sslSocket;
         return;
     }
@@ -57,16 +63,16 @@ void SslServer::incomingConnection(qintptr socketDescriptor)
     }
 
 
-    connect(sslSocket, &QSslSocket::encrypted,    this, &SslServer::onEncrypted);
-    connect(sslSocket, &QSslSocket::readyRead,    this, &SslServer::onReadyRead);
+    connect(sslSocket, &QSslSocket::encrypted, this, &SslServer::onEncrypted);
+    connect(sslSocket, &QSslSocket::readyRead, this, &SslServer::onReadyRead);
     connect(sslSocket, &QSslSocket::disconnected, this, &SslServer::onDisconnected);
-    connect(sslSocket,  QOverload<const QList<QSslError>&>::of(&QSslSocket::sslErrors),
+    connect(sslSocket, QOverload<const QList<QSslError>&>::of(&QSslSocket::sslErrors),
             this, &SslServer::onSslErrors);
 
 
     sslSocket->startServerEncryption();
 
-
+    qDebug() << "New connection: waiting for SSL handshake...";
 }
 
 void SslServer::onEncrypted()
@@ -74,10 +80,11 @@ void SslServer::onEncrypted()
     QSslSocket *socket = qobject_cast<QSslSocket*>(sender());
     if (!socket) return;
 
-
+    qDebug().nospace() << "SSL handshake is successful! The client is connected:"
+                       << socket->peerAddress().toString() << ":" << socket->peerPort();
 
     m_clients.append(socket);
-
+    socket->write("Hi");
 }
 
 void SslServer::onReadyRead()
@@ -92,13 +99,34 @@ void SslServer::onReadyRead()
         QString message = QString::fromUtf8(data).trimmed();
         qInfo()<< "Message from" << socket->peerAddress().toString() << ":" << message;
 
+        QJsonParseError parseError;
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(message.toUtf8(), &parseError);
 
-        QByteArray broadcast = data;  /
-        for (QSslSocket *client : qAsConst(m_clients)) {
-            if (client->state() == QAbstractSocket::ConnectedState) {
-                client->write(broadcast);
-            }
+        if (parseError.error != QJsonParseError::NoError) {
+            qWarning() << "JSON parse error:" << parseError.errorString();
+            return;
         }
+
+
+        if (!jsonDoc.isObject()) {
+            qWarning() << "JSON is not an object";
+            return;
+        }
+
+        QJsonObject jsonObj = jsonDoc.object();
+
+
+        QString type = jsonObj.value("type").toString();
+
+
+
+        if (type == "authorization") {
+            qInfo()<<"authorization";
+        } else if (type == "command") {
+
+        }
+
+
     }
 }
 
@@ -107,7 +135,7 @@ void SslServer::onDisconnected()
     QSslSocket *socket = qobject_cast<QSslSocket*>(sender());
     if (!socket) return;
 
-    qInfo() << "the client has disconnected" << socket->peerAddress().toString();
+    qInfo() << "The client has disconnected:" << socket->peerAddress().toString();
     m_clients.removeOne(socket);
     socket->deleteLater();
 }
