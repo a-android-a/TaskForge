79721a4a9e8edc731efd2d08a39647c77851440f
a_android_a <androidnal@yandex.ru>
2026-03-18 15:45:26 +0300

    feat: extend ApiClient with task operations and signals
    - Add getAllTasks() and updateTaskStatus() methods
    - Add tasksReceived(QJsonArray) signal for task data
    - Add createWorkerWindow signal for post-login navigation
    - Include Task.h and QJsonArray
    - Complete client API for task management

--- Client/network/ApiClient.h
+++ Client/network/ApiClient.h
@@ -2,8 +2,11 @@
 
 #include <QSslSocket>
 #include <QString>
-#include"../Config.h"
+#include "../Config.h"
 #include "../../Server/User.h"
+#include "../../Server/Task.h"
+
+#include <QJsonArray>
 class ApiClient:public QSslSocket{
     Q_OBJECT;
 private:
@@ -18,9 +21,13 @@ public:
     void sendToServer (const QString &data);
     void connectToServer();
     void authenticate(const QString &login, const QString password);
+    void getAllTasks();
+    void updateTaskStatus(const int taskID, const int newTask);
 signals:
     void authorizationFailed();
     void authorizationOk();
+    void tasksReceived(const QJsonArray &TasksArry);
+    void createWorkerWindow();
 private slots:
     void slotReadyRead();
 
