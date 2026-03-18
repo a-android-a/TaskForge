aa0be0480c3d1d8341dd6de5bd6025e75e94ded4
a_android_a <androidnal@yandex.ru>
2026-03-18 15:28:03 +0300

    fix: add missing Q_OBJECT and includes to MainWindowWorker

--- Client/MainWindowWorker.h
+++ Client/MainWindowWorker.h
@@ -1,7 +1,30 @@
 #pragma once
 #include <QWidget>
+#include "network/ApiClient.h"
+#include "../Server/Task.h"
+#include "TaskColumn.h"
+#include <QJsonArray>
+class QTextEdit;
+class QLineEdit;
+class QPushButton;
+class QVBoxLayout;
+class QHBoxLayout;
 
 class MainWindowWorker: public QWidget
 {
-
+    Q_OBJECT;
+private:
+    ApiClient*   m_apiClient = nullptr;
+    QVBoxLayout* m_pVBoxLayout;
+    TaskColumn*  todoColumn;
+    TaskColumn*  inProgress;
+    TaskColumn*  doneColumn;
+    QPushButton* m_pLogoutButton;
+private slots:
+    void onTaskDropped(TaskCard* card, TaskColumn* targetColumn);
+    void onTasksReceived(const QJsonArray &tasksArray);
+public:
+    explicit MainWindowWorker(QWidget *parent = nullptr);
+    void setApiClient(ApiClient* apiClient);
+    void setTasks(QVector<Task> tasks);
 };
