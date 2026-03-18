86d8cfe645822ed1e3e90694c5e819a06a3dfa4d
a_android_a <androidnal@yandex.ru>
2026-03-18 15:58:15 +0300

    refactor: improve TaskDatabaseManager.h with better types and naming

--- Server/TaskDatabaseManager.h
+++ Server/TaskDatabaseManager.h
@@ -7,9 +7,9 @@ class TaskDatabaseManager: public BaseDatabaseManager{
 public:
     explicit TaskDatabaseManager(const QString& path = "DB/tasks.db"): BaseDatabaseManager(path, "task_conn") {}
 
-    Task getTaskById(int id);
+    Task getTaskById(qint64 id);
     QVector<Task> getAllTasks();
-    bool createTask(const Task& ts);
-    bool updateTask(const qint64 id, const Task &ts);
+    bool createTask(const Task& task);
+    bool updateTask(const qint64 id, const Task &task);
 
-}
+};
