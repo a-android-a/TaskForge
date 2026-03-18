dd0cd5b9caf92e946a9bef3963219c955761c95d
a_android_a <androidnal@yandex.ru>
2026-03-18 15:41:23 +0300

    feat: create ApiClient in main and inject into auth window

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "UserAuthWindow.h"
+#include "network/ApiClient.h"
 #include <QApplication>
-
+#include <QString>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    UserAuthWindow auth(nullptr);
+    ApiClient* apiClient = new ApiClient(&a);
+    QString host = "127.0.0.1";
+
+
+    UserAuthWindow auth(apiClient, nullptr);
     auth.show();
     return a.exec();
 }
