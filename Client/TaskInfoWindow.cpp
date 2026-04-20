728b576f1b2f3aab03cf6d2ed74d46570cda30e2
a_android_a <androidnal@yandex.ru>
2026-04-20 12:21:46 +0300

    feat: add TaskInfoWindow with subtasks and progress
    - Display task name, due date, priority, status
    - Parse JSON description with main text and subtasks
    - Show subtasks as checkboxes
    - Update progress bar based on completed subtasks

--- Client/TaskInfoWindow.cpp
+++ Client/TaskInfoWindow.cpp
@@ -1,5 +1,159 @@
-#include"TaskInfoWindow.h"
+#include "TaskInfoWindow.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTextEdit>
+#include <QCheckBox>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QJsonParseError>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QScrollArea>
+#include <QFile>
+TaskInfoWindow::TaskInfoWindow(QWidget *parent,const Task &task)
+    : QWidget(parent),m_task(task)
+{
+    setWindowTitle(tr("Task Information"));
+    resize(520, 620);
 
-TaskInfoWindow::TaskInfoWindow(QWidget *parent) : QWidget(parent){
+    mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(20, 20, 20, 20);
+    mainLayout->setSpacing(15);
 
+    // Заголовок
+    QLabel *titleLabel = new QLabel(tr("Task name: ") + task.taskName, this);
+    mainLayout->addWidget(titleLabel);
+
+    // Описание
+    QLabel *descLabel = new QLabel(tr("Description:" ) , this);
+    mainLayout->addWidget(descLabel);
+
+    descriptionEdit = new QTextEdit(this);
+    descriptionEdit->setMinimumHeight(120);
+    descriptionEdit->setReadOnly(true);
+    mainLayout->addWidget(descriptionEdit);
+
+    // Дедлайн
+    QLabel *dueLabel = new QLabel(tr("Due date: ") +  task.due_date, this);
+    mainLayout->addWidget(dueLabel);
+
+    // Приоритет
+    QLabel *priorityLabel = new QLabel(tr("Priority:") + QString::number(task.priority), this);
+    mainLayout->addWidget(priorityLabel);
+
+    // Статус
+    QLabel *statusLabel = new QLabel(tr("Status: ") + QString::number(task.status), this);
+    mainLayout->addWidget(statusLabel);
+
+    // Прогресс
+    QLabel *progressLabel = new QLabel(tr("Progress:"), this);
+    mainLayout->addWidget(progressLabel);
+
+    progressBar = new QProgressBar(this);
+    progressBar->setRange(0, 100);
+    progressBar->setValue(0);
+    mainLayout->addWidget(progressBar);
+
+    // 🔹 Скроллируемая область для чекбоксов
+    scrollArea = new QScrollArea(this);
+    scrollArea->setWidgetResizable(true);
+    mainLayout->addWidget(scrollArea);
+
+    // Внутренний виджет для чекбоксов
+    QWidget *scrollWidget = new QWidget(this);
+    LayoutCheckBox = new QVBoxLayout(scrollWidget);
+    scrollWidget->setLayout(LayoutCheckBox);
+
+    scrollArea->setWidget(scrollWidget);
+
+    // Кнопки
+    QHBoxLayout *btnLayout = new QHBoxLayout();
+    QPushButton *saveBtn   = new QPushButton(tr("Save"), this);
+    QPushButton *closeBtn  = new QPushButton(tr("Close"), this);
+    saveBtn->setObjectName("saveButton");
+    closeBtn->setObjectName("closeButton");
+
+    btnLayout->addWidget(saveBtn);
+    btnLayout->addWidget(closeBtn);
+    mainLayout->addLayout(btnLayout);
 }
+
+void TaskInfoWindow::setDescription(const QString &desc)
+{
+    description = desc;
+
+    // Парсим JSON
+    QJsonParseError err;
+    QJsonDocument doc = QJsonDocument::fromJson(desc.toUtf8(), &err);
+
+    if (err.error != QJsonParseError::NoError) {
+        descriptionEdit->setText("Ошибка парсинга JSON: " + err.errorString());
+        return;
+    }
+
+    if (!doc.isObject()) {
+        descriptionEdit->setText("Некорректный JSON формат");
+        return;
+    }
+
+    QJsonObject obj = doc.object();
+
+    // 1. Основной текст
+    QString mainText = obj.value("main").toString();
+
+    // 2. Подзадачи
+    QString subtasksText;
+    QJsonArray tasksArray = obj.value("tasks").toArray();
+
+    for (const QJsonValue &v : tasksArray) {
+        QJsonObject t = v.toObject();
+
+        bool done = t.value("done").toBool();
+        //QString text = t.value("text").toString();
+
+        QCheckBox* checkBox = new QCheckBox(this);
+        checkBox->setText(t.value("text").toString());
+        LayoutCheckBox->addWidget(checkBox);
+        subtaskCheckboxes.append(checkBox);
+        connect(checkBox, &QCheckBox::stateChanged, this, [this](){
+            updateProgress();
+        });
+    }
+
+    // 3. Формируем вывод
+
+    // 4. Показываем в QTextEdit
+    updateProgress();
+    descriptionEdit->setText(mainText);
+}
+void TaskInfoWindow::updateProgress()
+{
+    int total = subtaskCheckboxes.size();
+    if (total == 0) {
+        progressBar->setValue(0);
+        return;
+    }
+
+    int doneCount = 0;
+    for (QCheckBox *cb : subtaskCheckboxes) {
+        if (cb->isChecked())
+            doneCount++;
+    }
+
+    int percent = (doneCount * 100) / total;
+    progressBar->setValue(percent);
+}
+bool TaskInfoWindow::setStyle(const QString &styleFileName){
+
+    QFile file(styleFileName);
+    if(file.open(QFile::ReadOnly)){
+        QString strCSS = QLatin1String(file.readAll());
+        this->setStyleSheet(strCSS);
+        file.close();
+        return true;
+    }
+    return false;
+}
+
--- Client/TaskInfoWindow.h
+++ Client/TaskInfoWindow.h
@@ -1,11 +1,43 @@
 #pragma once
-#include<QWidget>
+#include <QWidget>
+#include <QString>
+#include <QVector>
+#include "../Server/Task.h"
+class QLabel;
+class QCheckBox;
 class QVBoxLayout;
+class QTextEdit;
+class QProgressBar;
+class QScrollArea;
+
 class TaskInfoWindow:public QWidget{
     Q_OBJECT;
 private:
     QVBoxLayout* VBox;
+    Task m_task;
+    QString description;
+    qint64 m_taskId;
+
+    QLabel       *titleLabelValue;
+    QTextEdit    *descriptionEdit;
+    QLabel       *dueDateLabel;
+    QLabel       *priorityValue;
+    QCheckBox    *statusNotStarted;
+    QCheckBox    *statusInProgress;
+    QCheckBox    *statusDone;
+    QProgressBar *progressBar;
+    QVBoxLayout  *mainLayout;
+    QVBoxLayout  *LayoutCheckBox;
+    QScrollArea *scrollArea;
+    QWidget *scrollWidget;
+    QVector<QCheckBox*> subtaskCheckboxes;
+
+    void updateProgress();
+
+
 public:
-   explicit TaskInfoWindow(QWidget *parent = nullptr);
+    explicit TaskInfoWindow(QWidget *parent = nullptr, const Task &task=  Task());
+    void setDescription(const QString &desc);
+    bool setStyle(const QString &styleFileName);
 
 };
