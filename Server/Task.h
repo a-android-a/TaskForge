41a153285cbdf0508116ef486af9cf16f381e9e9
a_android_a <androidnal@yandex.ru>
2026-04-20 12:20:51 +0300

    refactor: remove description field from Task struct
    - Description will be stored separately and loaded on-demand
    - Reduces data transfer when fetching task lists

--- Server/Task.h
+++ Server/Task.h
@@ -6,7 +6,7 @@ struct Task
     qint64  status;
     qint64  priority;
     QString taskName;
-    QString description;
+    //QString description;
     QString due_date;
     QString created_by;
     QString assigned_to;
