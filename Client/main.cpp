77721bdf97cf2407a93465f6f6857dd8b5188405
a_android_a <androidnal@yandex.ru>
2026-04-28 16:10:03 +0300

    feat: load style path from Config in main

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,17 +1,18 @@
 #include "mainwindow.h"
 #include "UserAuthWindow.h"
 #include "network/ApiClient.h"
+#include "Config.h"
 #include <QApplication>
 #include <QString>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ApiClient* apiClient = new ApiClient(&a);
-    QString host = "127.0.0.1";
+
 
 
     UserAuthWindow auth(apiClient, nullptr);
     auth.show();
-    auth.setStyle("style/stylesMainWindowWorkerLight.qss");
+    auth.setStyle(Config::instance().getStylePath());
     return a.exec();
 }
