f5e1db4fee14aa2f3bd79cd98a70da30f7c487ab
a_android_a <androidnal@yandex.ru>
2026-04-29 15:06:05 +0300

    feat: add dynamic language switching

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,10 +4,25 @@
 #include "Config.h"
 #include <QApplication>
 #include <QString>
+#include <QTranslator>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ApiClient* apiClient = new ApiClient(&a);
+    Config::instance().load();
+    QString lang = Config::instance().getLanguage(); // "ru" или "en"
+
+    static QTranslator translator;
+
+    QString path = QCoreApplication::applicationDirPath()
+                   + "/translations/app_" + lang + ".qm";
+
+
+    if (translator.load(path)) {
+        a.installTranslator(&translator);
+    } else {
+        qWarning() << "Не удалось загрузить перевод:" << path;
+    }
 
 
 
