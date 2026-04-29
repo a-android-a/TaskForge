ecee79cec15f27e5b07a8d536b074c14b0fba7ef
a_android_a <androidnal@yandex.ru>
2026-04-29 13:10:24 +0300

    feat: add translations file

---
+++ Client/translations/app_ru.ts
@@ -0,0 +1,358 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>ApiClient</name>
+    <message>
+        <location filename="../network/ApiClient.cpp" line="61"/>
+        <source>Certificate Error</source>
+        <translation>Ошибка сертификата</translation>
+    </message>
+    <message>
+        <location filename="../network/ApiClient.cpp" line="62"/>
+        <source>Failed to load server certificate (server.crt).
+Please ensure the file is in the application directory.</source>
+        <translation>Не удалось загрузить сертификат сервера (server.crt). Пожалуйста, убедитесь, что файл находится в каталоге приложения.</translation>
+    </message>
+    <message>
+        <location filename="../network/ApiClient.cpp" line="76"/>
+        <source>Failed to connect to server:
+%1</source>
+        <translation>Не удалось подключиться к серверу:\n%1</translation>
+    </message>
+    <message>
+        <location filename="../network/ApiClient.cpp" line="77"/>
+        <source>Connection Error</source>
+        <translation>Ошибка соединения</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowAdmin</name>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="42"/>
+        <source>Admin</source>
+        <translation>Администратор</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="48"/>
+        <source>log out</source>
+        <translation>выйти</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="57"/>
+        <source>Admin Panel</source>
+        <translation>Панель админа</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="75"/>
+        <source>Create a user</source>
+        <translation>Создать пользователя</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="76"/>
+        <source>Unbanking a user/Ban a user</source>
+        <translation>Разблокировка пользователя/Бан пользователя</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="201"/>
+        <source>Create a task</source>
+        <translation>Создать задачу</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="202"/>
+        <source>Delete an issue</source>
+        <translation>Удалить задачу</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="203"/>
+        <source>Edit a task</source>
+        <translation>Редактировать задачу</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowAdmin.cpp" line="204"/>
+        <source>Task list</source>
+        <translation>Список задач</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowManager</name>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="15"/>
+        <source>SaveTask</source>
+        <translation>Сохранить задачу</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="16"/>
+        <source>addTask</source>
+        <translation>Добавить задачу</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="17"/>
+        <source>task list</source>
+        <translation>Список задач</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="86"/>
+        <source>New subtask</source>
+        <translation>Новая подзадача</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="147"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowManager.cpp" line="147"/>
+        <source>all fields must be filled in</source>
+        <translation>все поля должны быть заполнены</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowWorker</name>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="32"/>
+        <source>User</source>
+        <translation>Пользователь</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="39"/>
+        <source>Connection...</source>
+        <translation>Соединение...</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="45"/>
+        <source>log out</source>
+        <translation>выйти</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="46"/>
+        <source>Reconnect</source>
+        <translation>Повторно подключиться</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="66"/>
+        <source>Not started</source>
+        <translation>Не начато</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="67"/>
+        <source>At work</source>
+        <translation>В работе</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="68"/>
+        <source>Completed</source>
+        <translation>Завершенно</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="113"/>
+        <source>Task info</source>
+        <translation>Информация о задаче</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="147"/>
+        <location filename="../MainWindowWorker.cpp" line="153"/>
+        <source>Connected</source>
+        <translation>подключенно</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowWorker.cpp" line="158"/>
+        <source>Connection error</source>
+        <translation>Ошибка соединения</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../SettingsWindow.cpp" line="17"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>TaskDeletedWindow</name>
+    <message>
+        <location filename="../TaskDeletedWindow.cpp" line="19"/>
+        <source>Select the task to delete:</source>
+        <translation>Выберите задачу для удаления:</translation>
+    </message>
+    <message>
+        <location filename="../TaskDeletedWindow.cpp" line="33"/>
+        <source>Delete a selected task</source>
+        <translation>Удалить выбранную задачу</translation>
+    </message>
+</context>
+<context>
+    <name>TaskInfoWindow</name>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="18"/>
+        <source>Task Information</source>
+        <translation>Информация о задаче</translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="26"/>
+        <source>Task name: </source>
+        <translation>Название задачи: </translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="30"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="39"/>
+        <source>Due date: </source>
+        <translation>Срок погашения: </translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="43"/>
+        <source>Priority:</source>
+        <translation>Приоритет:</translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="47"/>
+        <source>Status: </source>
+        <translation>Статус: </translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="50"/>
+        <source>Progress:</source>
+        <translation>Прогресс:</translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="71"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../TaskInfoWindow.cpp" line="72"/>
+        <source>Close</source>
+        <translation>Закрывать</translation>
+    </message>
+</context>
+<context>
+    <name>UserAuthWindow</name>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="26"/>
+        <source>Enter</source>
+        <translation>Входить</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="27"/>
+        <source>Reconnect</source>
+        <translation>Повторно подключиться</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="47"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="49"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="101"/>
+        <location filename="../UserAuthWindow.cpp" line="109"/>
+        <source>Missing information</source>
+        <translation>Недостающая информация</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="101"/>
+        <source>Both login and password fields are required</source>
+        <translation>Поля для входа и пароля обязательны для заполнения</translation>
+    </message>
+    <message>
+        <location filename="../UserAuthWindow.cpp" line="109"/>
+        <source>login or password incorrect</source>
+        <translation>неверный логин или пароль</translation>
+    </message>
+</context>
+<context>
+    <name>UserManagementWindow</name>
+    <message>
+        <location filename="../UserManagementWindow.cpp" line="20"/>
+        <source>Block</source>
+        <translation>Блок</translation>
+    </message>
+    <message>
+        <location filename="../UserManagementWindow.cpp" line="21"/>
+        <source>Unblock</source>
+        <translation>Разблокировать</translation>
+    </message>
+    <message>
+        <location filename="../UserManagementWindow.cpp" line="22"/>
+        <source>Update</source>
+        <translation>Обновление</translation>
+    </message>
+    <message>
+        <location filename="../UserManagementWindow.cpp" line="41"/>
+        <source>List of users</source>
+        <translation>Список пользователей</translation>
+    </message>
+</context>
+<context>
+    <name>WindowCreateUser</name>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="17"/>
+        <source>Creating a user</source>
+        <translation>Создание пользователя</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="22"/>
+        <source>Creating a new user</source>
+        <translation>Создание нового пользователя</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="35"/>
+        <source>name</source>
+        <translation>имя</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="36"/>
+        <source>surname</source>
+        <translation>фамилия</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="37"/>
+        <source>job title</source>
+        <translation>должность</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="38"/>
+        <source>login</source>
+        <translation>авторизоваться</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="39"/>
+        <source>passwor</source>
+        <translation>пароль</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="48"/>
+        <source>create</source>
+        <translation>создать</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="51"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="54"/>
+        <source>generate login password</source>
+        <translation>сгенерировать пароль и логин для входа</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="75"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../WindowCreateUser.cpp" line="75"/>
+        <source>Fill in all required fields!</source>
+        <translation>Заполните все обязательные поля!</translation>
+    </message>
+</context>
+</TS>
